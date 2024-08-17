c9ba068cf9b5c254f91f68478d4450871e9c7fab
Rana <rana.ahammed.012@gmail.com>
2024-08-17 15:47:32 +0600

    .

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -5,12 +5,10 @@ const nextConfig = {
       {
         protocol: 'http',
         hostname: 'localhost',
-        // port: '8000',
       },
       {
         protocol: 'https',
         hostname: 'intern-project-3uzm.onrender.com',
-        // port: '8000',
       },
     ],
   },
